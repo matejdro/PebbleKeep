001ac2c8e911489ca9201583502319cf58057a24
Matej Drobnič <matejdro@gmail.com>
2016-06-21 11:21:49 +0200

    Root check when opening main window.

--- app/src/main/java/com/matejdro/pebblekeep/MainActivity.java
+++ app/src/main/java/com/matejdro/pebblekeep/MainActivity.java
@@ -1,7 +1,12 @@
 package com.matejdro.pebblekeep;
 
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.matejdro.pebblecommons.util.RootUtil;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
+
+        if (!RootUtil.isDeviceRooted() || !RootUtil.isRootAccessible())
+        {
+            new AlertDialog.Builder(this)
+                    .setTitle(R.string.error_device_not_rooted)
+                    .setMessage(R.string.error_not_rooted_message)
+                    .setPositiveButton(R.string.button_exit, null)
+                    .setOnDismissListener(new DialogInterface.OnDismissListener()
+                    {
+                        @Override
+                        public void onDismiss(DialogInterface dialog)
+                        {
+                            finish();
+                        }
+                    })
+                    .show();
+        }
     }
 }
